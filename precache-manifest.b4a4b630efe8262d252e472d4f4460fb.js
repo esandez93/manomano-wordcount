564245862c0e7c2412e99c1747e73e6ba5fa35ca
Eric Sández <esandez93@gmail.com>
2019-01-27 14:27:11 +0100

    Updates

---
+++ precache-manifest.b4a4b630efe8262d252e472d4f4460fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8934e18c80b985a3c6e",
+    "url": "/manomano-wordcount/static/css/main.1bfa9116.chunk.css"
+  },
+  {
+    "revision": "a8934e18c80b985a3c6e",
+    "url": "/manomano-wordcount/static/js/main.a8934e18.chunk.js"
+  },
+  {
+    "revision": "1a596ca731a12bb19d37",
+    "url": "/manomano-wordcount/static/js/1.1a596ca7.chunk.js"
+  },
+  {
+    "revision": "a24e564cc629e5861c09",
+    "url": "/manomano-wordcount/static/js/runtime~main.a24e564c.js"
+  },
+  {
+    "revision": "25b2df25f8a0b9e92c0d368838c72160",
+    "url": "/manomano-wordcount/static/media/manomano-logo.25b2df25.svg"
+  },
+  {
+    "revision": "a02a329b5ee07556156feab753a05138",
+    "url": "/manomano-wordcount/index.html"
+  }
+];
